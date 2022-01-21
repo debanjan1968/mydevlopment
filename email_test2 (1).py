b1bef3a9b49004cb812122bad23e8b4e4764527b
Debanjan Chakraborty <debanjan.chakraborty@tatamotors.com>
2022-01-21 22:25:25 +0530

    Add the python file-2

---
+++ email_test2 (1).py
@@ -0,0 +1,21 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+# smtphost = "email-smtp.us-east-1.amazonaws.com"
+smtphost = "webmail.mail.us-east-1.awsapps.com"
+# Get the user name and password - from the IAM user that is created by SES.
+# password = "BHPwFenCeDAVG/ceWCcRauM/nNlX07rDxrGa/ZecPvgC"
+# username = "AKIA4AUWBYEHKXXTGRGM"
+password = "jigyasabot@2022"
+username = "jigyasabot@tmlbsl.com"
+message = "Test from Python via AuthSMTP"
+msg = MIMEMultipart()
+msg['From'] = "jigyasabot@tmlbsl.com"
+msg['To'] = "debanjanchakraborty@yahoo.co.in"
+msg['Subject'] = "Test from Python via AuthSMTP"
+msg.attach(MIMEText(message, 'plain'))
+server = smtplib.SMTP(smtphost)
+server.starttls()
+server.login(username, password)
+server.sendmail(msg['From'], msg['To'], msg.as_string())
+server.quit()
